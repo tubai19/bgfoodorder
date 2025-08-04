bdae8ccfcc8b37bf9188bc729897026e759cd2f3
tubai19 <97087712+tubai19@users.noreply.github.com>
2025-08-05 01:02:10 +0530

    Delete js/Checkout/Utility Modules directory

--- js/Checkout/Utility Modules/Delivery Utilities.js
+++
@@ -1,9 +0,0 @@
-import { AppState } from '../main.js';
-
-export function calculateDeliveryChargeByDistance(distance) {
-  if (distance <= 2) return 20;
-  if (distance <= 5) return 30;
-  if (distance <= 8) return 50;
-  if (distance <= AppState.MAX_DELIVERY_DISTANCE) return 80;
-  return null; // Beyond delivery range
-}
