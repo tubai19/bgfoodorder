3e5bc97bfe73dd3491ce99b0842f8e8150aaaadb
tubai19 <97087712+tubai19@users.noreply.github.com>
2025-08-18 12:23:09 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
